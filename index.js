7c14a9419b41da58ea7de2b9449be37e3c6bf1f2
Constanza Cabrera <constanza.cabrerao@gmail.com>
2023-12-10 18:03:45 -0300

    new repository

---
+++ index.js
@@ -0,0 +1,6 @@
+// var se puede cambiar el valor, let se puede modificar dentro de los () y cons no se deberia cambiar
+
+let precio = 400000;
+
+let precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
